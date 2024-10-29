106de7e9f4f3c7c89284e224db0c04820204c3b6
AndreasMacaveiu <andreas.macaveiu.4818@student.uu.se>
2024-10-29 17:35:47 +0100

    Python v6
    haha

--- pythracer v2.py
+++ pythracer v2.py
@@ -5,16 +5,15 @@ controloptions = {"r":"Try again", "q":"Quit"}
 difficultyoptions = {"easy":"simple", "medium":"complex", "hard":"Very complex"}
 easy = ["Atomic bent chetler är den bästa skidan","Rosignol sender free 110 är också bra","Coolaste skidåkaren är Sammy"]
 medium = ["sju sjö sjuka sjömen skjöt sjutton sjuka barn","Peter Piper picked a peck of pickled peppers. A peck of pickled peppers Peter Piper picked. If Peter Piper picked a peck of pickled peppers,Where’s the peck of pickled peppers Peter Piper picked?"]
-hard = ["I en känd essä med titeln ”Den moderna pessimismen och dess vedersakare” skriver Karl Vennberg om hur illa människans djupare behov och rikare möjligheter passar ett framstegsideal dikterat av förnuftskult: ”Människan som bihang till framgången, de ekonomiska förändringarna, den psykologiska analysen bar sig ungefär lika bakvänt åt som den gamla människan, som bara var ett bihang till en vidskepelse, en religion eller en idé.” Essän trycktes 1946 i tidskriften 40-tal, men framfördes redan hösten 1945 som en så kallad diskussionsinledning. (Dess ambitionsnivå förstummade möjligen en och annan i auditoriet.) I skuggan av kriget och atombombernas svampmoln försöker Vennberg med hela sin ironiska förvandlingskonst komma bröstgänges med den amorfa frågan om ändamål och medel i samhället och konsten.", "hej", "11111111111111111111111111111111111111I111111111"]
+hard = ["I en känd essä med titeln ”Den moderna pessimismen och dess vedersakare” skriver Karl Vennberg om hur illa människans djupare behov och rikare möjligheter passar ett framstegsideal dikterat av förnuftskult: ”Människan som bihang till framgången, de ekonomiska förändringarna, den psykologiska analysen bar sig ungefär lika bakvänt åt som den gamla människan, som bara var ett bihang till en vidskepelse, en religion eller en idé.” Essän trycktes 1946 i tidskriften 40-tal, men framfördes redan hösten 1945 som en så kallad diskussionsinledning. (Dess ambitionsnivå förstummade möjligen en och annan i auditoriet.) I skuggan av kriget och atombombernas svampmoln försöker Vennberg med hela sin ironiska förvandlingskonst komma bröstgänges med den amorfa frågan om ändamål och medel i samhället och konsten.", "hej"]
+impossible = ["Hegemoni", "Impertinent", ]
 stats = {"Time":"","Accuracy":""}
-choiceoptions = {"y": "Yes", "n": "No"}
 
 def splash():
     print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print( "          Pythracer                   ")
     print( "      TYPE OR DIEEEEEEEE              ")
     print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
 
 def choosetext (library): #Har viewtext har implementerat momenten med random ur en lista
     x = random.randint (0,len(library)-1)
@@ -37,21 +36,24 @@ def accuracy(text,library):
         else:
             fel += 1
             a+=1
-    print (f"Antal felstavade ord: {fel}")
+    return (fel)        
+                     #(f"Antal felstavade ord: {fel}")
     
 def view(description, strings):
     print(description)
     for x in strings:
         print(f"    {x})  {strings[x]}")
 
+def viewstats(description, strings):
+    print(description)
+    for x in range (0,len(strings)):
+        print(f"  {strings[x]}")
+
 def menu(title, prompt, options):
     view(title, options)
     action = input(prompt)
     if action in options:
        return action
-    else: 
-        print(f"{action} is not an option")
-    
 
 def select(title, prompt, options):
     view(title, options)
@@ -74,16 +76,7 @@ def select(title, prompt, options):
             elif action != "r" or "q": 
                 print("try, try again")
 
-def save_highscore(lista,filename="highscore.txt"):
-    action = menu("Would you like to be engraved eternally into THE Highscore", "Y/N: ", choiceoptions)
-    if action == "y":
-        name = input("What name would you like to be remembered as? ")
-        with open(filename, "w") as file:
-            file.write(name + str(lista))
-            print("Registered")
-    else:
-        print("You weren't good enough anyway")
-        
+
 def startgame():
     start_time = time.time()
     current = opt1
@@ -94,9 +87,8 @@ def startgame():
     final_time = end_time - start_time
     q = str(round(final_time,2))
     stats["Time"]=q
-    stats["Accuracy"] = accurate
+    stats["Typo(s)"] = accurate
     view("Stats; Time, accuracy", stats)
-
     
     
 splash()
@@ -104,4 +96,5 @@ opt1 = select("Select a Difficulty", "Difficulty selected: ", difficultyoptions)
 print(f"You selected Difficulty: {opt1}")
 print()
 startgame()
-save_highscore(stats)
+with open("Highscore.txt", "w") as file:
+    file.write(str(stats))
