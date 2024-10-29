218e01474b774830c4af48c91dc1060aee4ca7ba
AndreasMacaveiu <andreas.macaveiu.4818@student.uu.se>
2024-10-29 16:39:09 +0100

    Pythracer v5
    Working menu and everything

--- pythracer v5.py
+++ pythracer v5.py
@@ -1,9 +1,28 @@
 import random
+<<<<<<< Updated upstream
 options = {"r":"Try again", "q":"Quit"}
 difficultyoptions = {"easy":"simple", "medium":"complex", "hard":"Very complex"}
 easy = ["Atomic bent chetler är den bästa skidan","Rosignol sender free 110 är också bra","Coolaste skidåkaren är Sammy"]
 medium = ["sju sjö sjuka sjömen skjöt sjutton sjuka barn","Peter Piper picked a peck of pickled peppers. A peck of pickled peppers Peter Piper picked. If Peter Piper picked a peck of pickled peppers,Where’s the peck of pickled peppers Peter Piper picked?"]
 hard = ["I en känd essä med titeln ”Den moderna pessimismen och dess vedersakare” skriver Karl Vennberg om hur illa människans djupare behov och rikare möjligheter passar ett framstegsideal dikterat av förnuftskult: ”Människan som bihang till framgången, de ekonomiska förändringarna, den psykologiska analysen bar sig ungefär lika bakvänt åt som den gamla människan, som bara var ett bihang till en vidskepelse, en religion eller en idé.” Essän trycktes 1946 i tidskriften 40-tal, men framfördes redan hösten 1945 som en så kallad diskussionsinledning. (Dess ambitionsnivå förstummade möjligen en och annan i auditoriet.) I skuggan av kriget och atombombernas svampmoln försöker Vennberg med hela sin ironiska förvandlingskonst komma bröstgänges med den amorfa frågan om ändamål och medel i samhället och konsten.", "hej"]
+=======
+import time
+import sys
+controloptions = {"r":"Try again", "q":"Quit"}
+difficultyoptions = {"easy":"simple", "medium":"complex", "hard":"Very complex"}
+easy = ["Atomic bent chetler är den bästa skidan","Rosignol sender free 110 är också bra","Coolaste skidåkaren är Sammy"]
+medium = ["sju sjö sjuka sjömen skjöt sjutton sjuka barn","Peter Piper picked a peck of pickled peppers. A peck of pickled peppers Peter Piper picked. If Peter Piper picked a peck of pickled peppers,Where’s the peck of pickled peppers Peter Piper picked?"]
+hard = ["I en känd essä med titeln ”Den moderna pessimismen och dess vedersakare” skriver Karl Vennberg om hur illa människans djupare behov och rikare möjligheter passar ett framstegsideal dikterat av förnuftskult: ”Människan som bihang till framgången, de ekonomiska förändringarna, den psykologiska analysen bar sig ungefär lika bakvänt åt som den gamla människan, som bara var ett bihang till en vidskepelse, en religion eller en idé.” Essän trycktes 1946 i tidskriften 40-tal, men framfördes redan hösten 1945 som en så kallad diskussionsinledning. (Dess ambitionsnivå förstummade möjligen en och annan i auditoriet.) I skuggan av kriget och atombombernas svampmoln försöker Vennberg med hela sin ironiska förvandlingskonst komma bröstgänges med den amorfa frågan om ändamål och medel i samhället och konsten.", "hej", "11111111111111111111111111111111111111I111111111"]
+stats = {"Time":"","Accuracy":""}
+
+
+def splash():
+    print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print( "          Pythracer                   ")
+    print( "      TYPE OR DIEEEEEEEE              ")
+    print( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+>>>>>>> Stashed changes
 def choosetext (library): #Har viewtext har implementerat momenten med random ur en lista
     x = random.randint (0,len(library)-1)
     currentlist = library[x]
@@ -37,15 +56,19 @@ def menu(title, prompt, options):
     action = input(prompt)
     if action in options:
        return action
+<<<<<<< Updated upstream
     else: 
         print(f"{action} is not an option")
         opt1 = menu("Select a Difficulty", "Difficulty selected: ", difficultyoptions)  
     
+=======
+>>>>>>> Stashed changes
 
-def choosedifficulty(difficulty):
+def select(title, prompt, options):
+    view(title, options)
     while True:
-        diff = opt1
-        if diff in difficulty:
+        diff = input(prompt)
+        if diff in options:
             if diff == "easy":
                 return choosetext(easy) 
             elif diff == "medium":
@@ -53,6 +76,7 @@ def choosedifficulty(difficulty):
             elif diff == "hard":
                 return choosetext(hard)
         else:
+<<<<<<< Updated upstream
              action = menu("Invalid difficulty", "Option: ", options)
              if action == "q":
                 break
@@ -60,6 +84,34 @@ def choosedifficulty(difficulty):
 
 
 opt1 = menu("Select a Difficulty", "Difficulty selected: ", difficultyoptions)
+=======
+            print(f"{diff} is not an option")
+            action = menu("Invalid difficulty", "Option: ", controloptions)
+            if action == "q":
+                sys.exit("Quitting")
+            elif action == "r":
+                print("ok")
+            elif action != "r" or "q": 
+                print("try, try again")
+
+
+def startgame():
+    start_time = time.time()
+    current = opt1
+    print(current)
+    attempt = typing()
+    accurate = str(accuracy (attempt,current))
+    end_time = time.time()
+    final_time = end_time - start_time
+    q = str(round(final_time,2))
+    stats["Time"]=q
+    stats["Accuracy"] = accurate
+    view("Stats; Time, accuracy", stats)
+    
+    
+splash()
+opt1 = select("Select a Difficulty", "Difficulty selected: ", difficultyoptions)
+>>>>>>> Stashed changes
 print(f"You selected Difficulty: {opt1}")
 print()
 
